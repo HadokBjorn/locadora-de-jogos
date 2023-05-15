1baf3bbb5fe07dfe068dd0e196342335a8806fde
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-15 13:32:48 -0500

    fix: correction client schema

--- src/schemas/client.schema.js
+++ src/schemas/client.schema.js
@@ -2,8 +2,18 @@ import joi from "joi";
 
 const clientSchema = joi.object({
 	name: joi.string().min(3).max(100).required(),
-	phone: joi.string().min(10).max(11).required(),
-	cpf: joi.string().min(11).max(11).required(),
+	phone: joi
+		.string()
+		.min(10)
+		.max(11)
+		.pattern(/^[0-9]+$/)
+		.required(),
+	cpf: joi
+		.string()
+		.min(11)
+		.max(11)
+		.pattern(/^[0-9]+$/)
+		.required(),
 	birthday: joi
 		.date()
 		.max("01-01-2015")
