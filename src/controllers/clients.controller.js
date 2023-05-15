c35c3062bc2095b5b199a6c256d6cd74aeb106e5
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-15 14:57:23 -0500

    fix: case cpf conflic create customer

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -3,8 +3,8 @@ import { db } from "../database/database.connections.js";
 export async function createClient(req, res) {
 	const { name, phone, cpf, birthday } = req.body;
 	try {
-		/* const sameCpf = (await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])).rows.length;
-		if (sameCpf !== 0) return res.sendStatus(409); */
+		const sameCpf = (await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])).rows.length;
+		if (sameCpf !== 0) return res.sendStatus(409);
 
 		await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4)`, [
 			name,
